36bdfff31cc67460348bde9d8f136c5b13f510e4
samuel ogoigbe <ogoigbe12@gmail.com>
2021-06-18 03:06:54 -0700

    new

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,8 +19,7 @@ printf("and is greater than 5\n");
 else if (n % 10 == 0)
 printf("and is 0\n");
 else if (n % 10 < 6 && n % 10 != 0)
-printf("and is less than 6 and not 0\n"0);
+printf("and is less than 6 and not 0\n");
 return (0);
 }
-
 
