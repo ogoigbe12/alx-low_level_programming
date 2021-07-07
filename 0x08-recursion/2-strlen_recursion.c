62feba2a55b60e53b20751618aacc6e2307c54f8
samuel ogoigbe <ogoigbe12@gmail.com>
2021-07-07 02:03:59 -0700

    modified

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,9 +6,9 @@
  *
  * Return: length of the string
  */
-int_strlen_recursion(char *s)
+int _strlen_recursion(char *s)
 {
-if (*s == '\0')
-return (0);
-return (1 + _strlen_recursion(s + 1));
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
